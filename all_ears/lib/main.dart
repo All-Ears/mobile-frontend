18303095d9b21cf7bf5b4aa93d230d91de70c6cf
eam522 <eam522@nau.edu>
2021-02-28 15:32:35 -0700

    Added Prototype of Elephant Map and Carbon Calculator

--- all_ears/lib/main.dart
+++ all_ears/lib/main.dart
@@ -427,7 +427,6 @@ class _ThirdRoute extends State<ThirdRoute> with TickerProviderStateMixin {
     super.initState();
   }
 
-
   @override
   void dispos(){
     animationController?.dispose();
@@ -441,6 +440,98 @@ class _ThirdRoute extends State<ThirdRoute> with TickerProviderStateMixin {
     return Scaffold(
         appBar: AppBar(
           title: Text('Carbon Calculator'),
+        ),
+        drawer: Container(
+          width: 215.0,
+          child: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  Container(
+                    height: 110.0,
+                    child: DrawerHeader(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10.0, bottom: 0.0),
+                            child: ClipOval(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/');
+                                },
+                                child: Image(
+                                  width: 55,
+                                  height: 55,
+                                  fit: BoxFit.cover,
+                                  image: AssetImage('assets/images/highreslogo.png'),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                            child: Text(
+                              'AllEars',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.text_snippet_outlined,
+                        color: Colors.tealAccent[400]),
+                    title: Text('About Us'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/second');
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.calculate_outlined, color: Colors.red),
+                    title: Text('Carbon Calculator'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/third');
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.monetization_on_outlined,
+                        color: Colors.amber[600]),
+                    title: Text('Donate'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/fourth');
+                    },
+                  ),
+                  Container(
+                      child: ListTile(
+                        leading: Icon(Icons.map_outlined, color: Colors.green),
+                        title: Text('Elephant Map'),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/fifth');
+                        },
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border(bottom: BorderSide(color: Colors.black12)))),
+                  ListTile(
+                    title: Text('FAQ'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/sixth');
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Write a Review'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/');
+                    },
+                  ),
+                ],
+              )),
         ),
         body: LayoutBuilder(builder: (ctx, constraints) {
           return ListView(
@@ -515,22 +606,6 @@ class _ThirdRoute extends State<ThirdRoute> with TickerProviderStateMixin {
         )
     );
   }
-}
-
-
-class LineModel {
-  const LineModel(this.from, this.to);
-  final MapLatLng from;
-  final MapLatLng to;
-}
-
-double calculateLngLat(double lat1, double lng1, double lat2, double lng2){
-  var p = 0.017453292519943295;
-  var c = cos;
-  var a = 0.5 - c((lat2 - lat1) * p)/2 +
-      c(lat1 * p) * c(lat2 * p) *
-          (1 - c((lng2 - lng1) * p))/2;
-  return 12742 * asin(sqrt(a));
 }
 
 class FourthRoute extends StatelessWidget {
@@ -765,131 +840,16 @@ class FourthRoute extends StatelessWidget {
   }
 }
 
-class FifthRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Elephant Map",
-            style: TextStyle(
-              fontSize: 30,
-            )),
-        backgroundColor: Colors.lightGreen,
-      ),
-      drawer: Container(
-        width: 215.0,
-        child: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                Container(
-                  height: 110.0,
-                  child: DrawerHeader(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 10.0, bottom: 0.0),
-                          child: ClipOval(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/');
-                              },
-                              child: Image(
-                                width: 55,
-                                height: 55,
-                                fit: BoxFit.cover,
-                                image: AssetImage('assets/images/highreslogo.png'),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 12.0, right: 12.0),
-                          child: Text(
-                            'AllEars',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.text_snippet_outlined,
-                      color: Colors.tealAccent[400]),
-                  title: Text('About Us'),
-                  onTap: () {
-                    Navigator.pushNamed(context, '/second');
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.calculate_outlined, color: Colors.red),
-                  title: Text('Carbon Calculator'),
-                  onTap: () {
-                    Navigator.pushNamed(context, '/third');
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.monetization_on_outlined,
-                      color: Colors.amber[600]),
-                  title: Text('Donate'),
-                  onTap: () {
-                    Navigator.pushNamed(context, '/fourth');
-                  },
-                ),
-                Container(
-                    child: ListTile(
-                      leading: Icon(Icons.map_outlined, color: Colors.green),
-                      title: Text('Elephant Map'),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/fifth');
-                      },
-                    ),
-                    decoration: BoxDecoration(
-                        border: Border(bottom: BorderSide(color: Colors.black12)))),
-                ListTile(
-                  title: Text('FAQ'),
-                  onTap: () {
-                    Navigator.pushNamed(context, '/sixth');
-                  },
-                ),
-                ListTile(
-                  title: Text('Write a Review'),
-                  onTap: () {
-                    Navigator.pushNamed(context, '/');
-                  },
-                ),
-              ],
-            )),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back!'),
-        ),
-      ),
-    );
-  }
-}
-
-class SixthRoute extends StatefulWidget{
-  SixthRoute({Key key, this.title}) : super(key: key);
+class FifthRoute extends StatefulWidget{
+  FifthRoute({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _SixthRoute createState() => _SixthRoute();
+  _FifthRoute createState() => _FifthRoute();
 }
 
-class _SixthRoute extends State<SixthRoute> {
+class _FifthRoute extends State<FifthRoute> {
   int selectedIndex = -1;
   MapShapeSource forestElephantMapShapes;
   MapZoomPanBehavior _zoomPanBehavior;
@@ -1154,6 +1114,127 @@ class _SixthRoute extends State<SixthRoute> {
   }
 }
 
+class SixthRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frequently Asked Questions",
+            style: TextStyle(
+              fontSize: 30,
+            )),
+        backgroundColor: Colors.lightGreen,
+      ),
+      drawer: Container(
+        width: 215.0,
+        child: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                Container(
+                  height: 110.0,
+                  child: DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10.0, bottom: 0.0),
+                          child: ClipOval(
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.pushNamed(context, '/');
+                              },
+                              child: Image(
+                                width: 55,
+                                height: 55,
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/images/highreslogo.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                          child: Text(
+                            'AllEars',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.text_snippet_outlined,
+                      color: Colors.tealAccent[400]),
+                  title: Text('About Us'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/second');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.calculate_outlined, color: Colors.red),
+                  title: Text('Carbon Calculator'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/third');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.monetization_on_outlined,
+                      color: Colors.amber[600]),
+                  title: Text('Donate'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/fourth');
+                  },
+                ),
+                Container(
+                    child: ListTile(
+                      leading: Icon(Icons.map_outlined, color: Colors.green),
+                      title: Text('Elephant Map'),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/fifth');
+                      },
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border(bottom: BorderSide(color: Colors.black12)))),
+                ListTile(
+                  title: Text('FAQ'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/sixth');
+                  },
+                ),
+                ListTile(
+                  title: Text('Write a Review'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                ),
+              ],
+            )),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class LineModel {
+  const LineModel(this.from, this.to);
+  final MapLatLng from;
+  final MapLatLng to;
+}
+
 class MapModel {
   final String country;
   final int totalDeaths;
@@ -1170,5 +1251,14 @@ class DeathData {
   final int illegalCarcasses;
 
   const DeathData(this.countryName, this.countryCode, this.year, this.natureCarcasses, this.illegalCarcasses);
+}
+
+double calculateLngLat(double lat1, double lng1, double lat2, double lng2){
+  var p = 0.017453292519943295;
+  var c = cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 +
+      c(lat1 * p) * c(lat2 * p) *
+          (1 - c((lng2 - lng1) * p))/2;
+  return 12742 * asin(sqrt(a));
 }
 
